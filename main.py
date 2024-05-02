866a51b03e0ddf5a611c41c66bd014a560d25e3f
Hiteshj1693 <Hiteshjethava161611@gmail.com>
2024-05-02 10:41:13 +0530

    update

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 print("Start")
+
+# import PyPDF2
+
+# def extract_text_from_pdf(pdf_path):
+#     text = ""
+#     with open(pdf_path, 'rb') as file:
+#         reader = PyPDF2.PdfReader(file)
+#         num_pages = len(reader.pages)
+#         for page_num in range(num_pages):
+#             page = reader.pages[page_num]
+#             text += page.extract_text()
+#     return text
+
+# pdf_file_path = "/Users/nitinjethava/Downloads/Study/AI/AI-Resume-Analyzer/HiteshResume.pdf"
+# resume_text = extract_text_from_pdf(pdf_file_path)
+# print(resume_text)
+
+
+from pyresparser import ResumeParser
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, 'rb') as file:
+        parser = ResumeParser(file)
+        parsed_data = parser.get_extracted_data()
+        text = parsed_data["text"]
+    return text
+
+pdf_file_path = "/Users/nitinjethava/Downloads/Study/AI/AI-Resume-Analyzer/HiteshResume.pdf"
+resume_text = extract_text_from_pdf(pdf_file_path)
+print(resume_text)
